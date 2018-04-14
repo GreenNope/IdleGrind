8d122971d64b486ad8a040acdbee1d4805d4bf31
GreenNope <tlash1000@gmail.com>
2018-04-14 12:12:04 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,10 @@
-function checkStatCombo() {
+commonStatCombos = {
+{stat1: magDamage, stat2: atkSpeed}
+}
 
+
+
+function checkStatCombo() {
+    stat1 = document.getElementById(“stat1”)
+    stat2 = document.getElementById(“stat2”)
 }
