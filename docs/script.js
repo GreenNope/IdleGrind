af8aed60cd8fcdf630b991895c74c1fbe6790706
GreenNope <tlash1000@gmail.com>
2018-04-14 14:26:15 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -8,7 +8,7 @@ function checkStatCombo() {
     stat1 = document.getElementById(“stat1”)
     stat2 = document.getElementById(“stat2”)
     for (combo in commonStatCombos) {
-        if (combo.stat1 = stat1 and combo.stat2 = stat2) {
+        if (combo.stat1 = stat1 && combo.stat2 = stat2) {
             return true
         }
     }
